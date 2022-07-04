c8c971e12190dd9204193067c16a1ea1cb569356
Lans12345 <olanalans12345@gmail.com>
2022-07-04 14:09:22 +0800

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx/firstPage.dart';
 import 'controller.dart';
 import 'firebase_options.dart';
 
-void main() async {
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
+}
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 }
 
